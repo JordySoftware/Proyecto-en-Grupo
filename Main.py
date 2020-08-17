07ad2aba7a38ded686fd0691810c818453541373
Bryan Jácome <69658287+bryanjt2001@users.noreply.github.com>
2020-08-16 20:49:55 -0500

    commit 4
    Declaro un if el cual controlara si en caso en la variable userID  digitan la letra y si eso ocurre el programa finalizara.

--- Main.py
+++ Main.py
@@ -6,5 +6,10 @@ flag = True
 while flag:
   print("\n Bienvenido al Request de stack OverFlow")
   userid = input("\n Digite el ID del usuario de quien desea generar sus etiquetas o si desea salir digite y: ")
-
+  
+  if userid == "y" or userid == "Y":
+        flag = False
+        print("Saliendo.................")
+        break
+    
 
