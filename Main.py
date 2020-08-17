c5eeefa167bb6d3871ef26d60cb950056d5bf73e
Bryan Jácome <69658287+bryanjt2001@users.noreply.github.com>
2020-08-16 20:48:04 -0500

    commit 3
    Presento un mensaje de bienvenida y declaro una variable userId el cual almacenara el id de usuario.

--- Main.py
+++ Main.py
@@ -4,4 +4,7 @@ from UsersTags import getTags
 flag = True
 
 while flag:
+  print("\n Bienvenido al Request de stack OverFlow")
+  userid = input("\n Digite el ID del usuario de quien desea generar sus etiquetas o si desea salir digite y: ")
+
 
