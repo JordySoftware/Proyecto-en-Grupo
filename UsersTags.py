0cde404aeaa7130819416c127980157a97177cfa
lkevinluis <69775536+lkevinluis@users.noreply.github.com>
2020-08-16 21:07:11 -0500

    obtención  de datos
    se recoge los datos  del usuario y se lo almacena temporalmente

--- UsersTags.py
+++ UsersTags.py
@@ -2,4 +2,7 @@ import requests
 import json
 
 def getTags(user_id):
-url = f"https://api.stackexchange.com/2.2/users/{user_id}/top-tags?site=es.s
+    url = f"https://api.stackexchange.com/2.2/users/{user_id}/top-tags?site=es.s
+
+    htmlFormat = requests.get(url)
+    jsonFormat = htmlFormat.json()
